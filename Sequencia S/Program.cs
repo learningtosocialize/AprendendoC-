b367385b39949b5438ada00ee872da63cdac0a78
Leonardo <89586048+learningtosocialize@users.noreply.github.com>
2022-01-19 16:36:32 -0300

    Add files via upload

---
+++ Sequencia S/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class DIO
+{
+
+    static void Main(string[] args)
+    {
+        double S = 0;
+        
+        for (double i = 1; i <= 100; i++)
+        {
+            double c = 1;
+            double a = c / i;
+            S = S + a;
+        }
+        var x = Math.Round(S, 2);
+        Console.WriteLine(x);
+    }
+
+}

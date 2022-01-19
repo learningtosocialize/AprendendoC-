c284d5cf3e4eb056a013bc2f1d6f5932f7172320
Leonardo <89586048+learningtosocialize@users.noreply.github.com>
2022-01-19 16:35:00 -0300

    Add files via upload

---
+++ xenlongao/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class DIO
+{
+
+    static void Main(string[] args)
+    {
+        int num1 = int.Parse(Console.ReadLine());
+        int num2 = int.Parse(Console.ReadLine());
+        int cont = 0;
+        int qnt_esfera = 0;
+
+        if (num1 >= num2)
+        {
+            while (num2 <= num1)
+            {
+
+                for (int i = 1; i < num1 + 1; i++)
+                {
+                    if (num2 % i == 0)
+                    {
+                        cont++;
+                    }
+                }
+                if (cont % 2 == 0)
+                {
+                    qnt_esfera++;
+                }
+                num2++;
+                cont = 0;
+            }
+            Console.WriteLine(qnt_esfera);
+        }
+
+        else
+        {
+            
+            while (num1 <= num2)
+            {
+
+                for (int i = 1; i < num2 + 1; i++)
+                {
+                    if (num1 % i == 0)
+                    {
+                        cont++;
+                    }
+                }
+                if (cont % 2 == 0)
+                {
+                    qnt_esfera++;
+                }
+                num1++;
+                cont = 0;
+            }
+            Console.WriteLine(qnt_esfera);
+        }
+    }
+     
+}

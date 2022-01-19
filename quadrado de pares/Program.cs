f53bf9be9e75d25894919d5e92279eeeb6c40746
Leonardo <89586048+learningtosocialize@users.noreply.github.com>
2022-01-19 16:39:54 -0300

    Add files via upload

---
+++ quadrado de pares/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 2 == 0)
+            {
+                double d1 = Math.Pow(i, 2);
+                Console.WriteLine($"{i}^2 = {d1}");
+            }
+               
+        }
+
+    }
+
+}
